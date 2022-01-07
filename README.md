2228b3c4880b21434a4b1407470974d5ff30d084
victormusembi11 <victor.musembi11@gmail.com>
2022-01-07 12:06:36 -0500

    All Set and Ready

---
+++ README.md
@@ -0,0 +1,1 @@
+# i3-window-manager-config
---
+++ i3wm.py
@@ -0,0 +1,44 @@
+import subprocess
+import os
+import pwd
+
+commands = [
+	'sudo apt-get install i3 -y',
+	'sudo apt-get install pulseaudio-utils -y',
+	'sudo apt-get install playerctl -y',
+	'sudo apt-get install feh -y',
+	'wget -0 ~/Pictures/wallpaper.jpg https://images.hdqwalls.com/wallpapers/yosemite-valley-4k-hd.jpg',
+	'sudo apt-get install rofi',
+	'sudo apt-get install compton',
+	'sudo apt-get install i3blocks',
+	'mkdir ~/.i3',
+	'cp /etc/i3blocks.conf ~/.i3'
+]
+
+def run_commands(commands):
+
+	print('Installing the required packages...')
+
+	for command in commands:
+		proc_run = subprocess.run(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
+		print(proc_run.stdout.decode() + proc_run.stderr.decode())
+
+	print('Packages installed successfully...')
+
+
+def config_00(rf_path, wf_path):
+
+	with open(rf_path, 'r') as read_file:
+		f_contents = read_file.read()
+
+	with open(wf_path, 'w') as write_file:
+		write_file.write(f_contents)
+
+comp_uname = pwd.getpwuid(os.getuid()).pw_name
+
+run_commands(commands)
+
+print('Reading custom config files and writing to official config files...\n')
+
+config_00('config_files/i3blocks.conf', '/home/' + comp_uname + '/.i3/i3blocks.conf')
+config_00('config_files/config', '/home/' + comp_uname + '/.config/i3/config')
